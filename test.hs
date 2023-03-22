6124d662db7fe9e0bddfb9dd960072394ebe4341
underthere <cltobecl@outlook.com>
2023-03-22 21:19:18 +0800

    foldable in lib

--- test.hs
+++ test.hs
@@ -1,2 +1,23 @@
-thisid :: a -> a
-thisid a = a
+
+data Sum a b = First a | Second b deriving (Eq, Show)
+
+instance Functor (Sum a) where
+  fmap f (Second b) = Second (f b)
+  fmap _ (First a) = First a
+
+instance Applicative (Sum a) where
+    pure = Second
+    (First a) <*> _ = First a
+    (Second f) <*> something = fmap f something
+
+instance Monad (Sum a) where
+  return = pure
+  (First a) >>= _ = First a
+  (Second b) >>= f = f b
+
+twiceEven :: [Integer] -> [Integer]
+twiceEven xs = do
+  x <- xs
+  if even x
+    then [x*x, x*x]
+    else []
