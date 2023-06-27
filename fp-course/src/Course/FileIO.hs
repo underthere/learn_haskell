e8451fffab7a0d5701f454c7d5d034048effe452
underthere <cltobecl@outlook.com>
2023-06-27 11:05:06 +0800

    FileIO

--- fp-course/src/Course/FileIO.hs
+++ fp-course/src/Course/FileIO.hs
@@ -79,52 +79,57 @@ the contents of c
 
 -}
 
+sequenceMap :: Applicative f => (a -> f b) -> (List a -> f (List b))
+sequenceMap f = sequence . (<$>) f
+
+voidSequenceMap :: Applicative f => (a -> f b) -> (List a -> f ())
+voidSequenceMap f = void . sequenceMap f
+
 -- Given the file name, and file contents, print them.
 -- Use @putStrLn@.
 printFile ::
   FilePath
   -> Chars
   -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
+printFile filepath content =
+  putStrLn ("============ " ++ filepath) >> putStrLn content
+
 
 -- Given a list of (file name and file contents), print each.
 -- Use @printFile@.
 printFiles ::
   List (FilePath, Chars)
   -> IO ()
-printFiles =
-  error "todo: Course.FileIO#printFiles"
+printFiles = voidSequenceMap (uncurry printFile)
 
 -- Given a file name, return (file name and file contents).
 -- Use @readFile@.
 getFile ::
   FilePath
   -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+getFile filepath = lift2 (,) (return filepath) (readFile filepath)
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
 getFiles ::
   List FilePath
   -> IO (List (FilePath, Chars))
-getFiles =
-  error "todo: Course.FileIO#getFiles"
+getFiles = sequenceMap getFile
 
 -- Given a file name, read it and for each line in that file, read and print contents of each.
 -- Use @getFiles@ and @printFiles@.
 run ::
   FilePath
   -> IO ()
-run =
-  error "todo: Course.FileIO#run"
+run indexfile = readFile indexfile >>= getFiles . lines >>= printFiles
+
+runAll :: List FilePath -> IO ()
+runAll = voidSequenceMap run
 
 -- /Tip:/ use @getArgs@ and @run@
 main ::
   IO ()
-main =
-  error "todo: Course.FileIO#main"
+main = getArgs >>= runAll
 
 ----
 
